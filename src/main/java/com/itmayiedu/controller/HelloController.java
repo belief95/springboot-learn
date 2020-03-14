86975e68e5a6cc6158cae1a9c416f6bc4c90c39c
2013-20171229CU\Administrator <1578212884@qq.com>
2020-03-14 09:39:23 +0800

    springboot启动方式

---
+++ src/main/java/com/itmayiedu/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.itmayiedu.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("hello2")
+    public String hello2() {
+        return "hello2";
+    }
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloController.class, args);
+//    }
+}
