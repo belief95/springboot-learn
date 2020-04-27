50f7f27554d4963eea8c0d4f9b6d30a93d6b1454
2013-20171229CU\Administrator <1578212884@qq.com>
2020-04-27 13:18:25 +0800

    add code

--- src/main/java/com/itmayiedu/controller/HelloController.java
+++ src/main/java/com/itmayiedu/controller/HelloController.java
@@ -2,9 +2,9 @@ package com.itmayiedu.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 //@EnableAutoConfiguration
 public class HelloController {
